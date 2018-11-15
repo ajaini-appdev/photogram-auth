c253d178eda8cb5ec2ee4cead8834587b0320a5c
Ankit Jaini <ajaini@chicagobooth.edu>
2018-11-15 02:19:35 +0000

    users/id show page done (point 18)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
   
-  # Routes for the User resource:
-  
-  # READ
-  get("/users", { :controller => "users", :action => "index"})
-  get("/users/:id_to_show", { :controller => "users", :action => "show" })
-  
   # Routes for the Comment resource:
 
   # CREATE
@@ -66,5 +60,13 @@ Rails.application.routes.draw do
   root "photos#index"
 
   devise_for :users
+  
+  # Routes for the User resource:
+  
+  # READ
+  get("/users", { :controller => "users", :action => "index"})
+  get("/users/:id_to_show", { :controller => "users", :action => "show" })
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
